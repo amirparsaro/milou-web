f2a9bdc957b6170c26bd7a0d7ea6c77c0f4644a7
amirparsaro <amirparsarouhani@gmail.com>
2025-06-01 14:50:38 +0330

    create tables messages & recipients in database

--- src/main/sql/tables.sql
+++ src/main/sql/tables.sql
@@ -6,3 +6,18 @@ create table users (
     email nvarchar(255) not null unique,
     password nvarchar(255) not null
 );
+
+create table messages (
+    id int primary key auto_increment,
+    title nvarchar(255) not null,
+    body nvarchar(6000) not null
+);
+
+create table Recipients (
+    id int primary key auto_increment,
+    message_id int not null,
+    user_id int not null,
+
+    foreign key (message_id) references messages(id),
+    foreign key (user_id) references users(id)
+);
