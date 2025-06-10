fc8296ed6fc5dac87bbe832d782fb526b8c9b9d5
amirparsaro <amirparsarouhani@gmail.com>
2025-06-11 00:48:12 +0330

    modify application to ignore db (no use for db in spring boot)

--- src/main/java/com/milou/spring_boot/Application.java
+++ src/main/java/com/milou/spring_boot/Application.java
@@ -2,19 +2,19 @@ package com.milou.spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
-@RestController
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @GetMapping
-    public String helloWorld() {
-        return "Hello World!";
-    }
 }
