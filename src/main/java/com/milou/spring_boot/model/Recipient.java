b8803d899e4af5a5bc7b0b437e3a8cbe80874ca0
amirparsaro <amirparsarouhani@gmail.com>
2025-06-12 10:49:22 +0330

    add empty constructor for recipient

--- src/main/java/com/milou/spring_boot/model/Recipient.java
+++ src/main/java/com/milou/spring_boot/model/Recipient.java
@@ -1,9 +1,10 @@
 package com.milou.spring_boot.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="recipients")
+@Table(name = "recipients")
 public class Recipient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -11,14 +12,19 @@ public class Recipient {
 
     @ManyToOne
     @JoinColumn(name = "message_id", nullable = false)
+    @JsonBackReference
     private Message message;
 
     @Basic(optional = false)
+    @Column(name = "is_read")
     private boolean read;
 
     @ManyToOne
     @JoinColumn(name = "recipient_id", nullable = false)
     private User recipient;
+
+    public Recipient() {
+    }
 
     public Recipient(Message message, User recipient) {
         this.message = message;
